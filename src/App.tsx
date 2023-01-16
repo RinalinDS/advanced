38171d9c0597b77e270a27bc2f338ecb5b50c4f6
RinalinDS <denispilyutin@gmail.com>
2023-01-17 01:45:09 +0200

    feature: useEffect for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AppRouter} from "./components/AppRouter";
 import {NavBar} from "./components/Navbar";
 import {Layout} from "antd";
+import {useActions} from "./hooks/useActions";
+import {IUser} from "./models/IUser";
 
 export const App = () => {
+    const {setUser, setIsAuth} = useActions()
+    useEffect(() => {
+        const isAuth = localStorage.getItem('auth')
+        if (isAuth) {
+            setUser({username: localStorage.getItem('username' || '')} as IUser)
+            setIsAuth(true);
+        }
+    }, [])
     return (
         <Layout>
             <NavBar/>
