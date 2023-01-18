69b661079973fa7e9080aa2dacf1b8bea8b6e323
RinalinDS <denispilyutin@gmail.com>
2023-01-18 11:11:16 +0200

    refactor: delete isLoading from cases

--- src/store/reducers/auth/index.ts
+++ src/store/reducers/auth/index.ts
@@ -11,13 +11,13 @@ const initialState: AuthState = {
 export const authReducer = (state: AuthState = initialState, action: AuthActions): AuthState => {
     switch (action.type) {
         case AuthActionsEnum.SET_IS_AUTH:
-            return {...state, isAuth: action.payload, isLoading: false}
+            return {...state, isAuth: action.payload}
         case AuthActionsEnum.SET_ERROR:
-            return {...state, error: action.payload, isLoading: false}
+            return {...state, error: action.payload}
         case AuthActionsEnum.SET_IS_LOADING:
             return {...state, isLoading: action.payload}
         case AuthActionsEnum.SET_USER:
-            return {...state, user: action.payload, isLoading: false}
+            return {...state, user: action.payload}
         default:
             return state
     }
